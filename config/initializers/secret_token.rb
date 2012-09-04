2aae1aa2846f380d0b77507498069c880484f285
quchen <1987quchen@gmail.com>
2012-09-04 22:18:42 +0800

    create the app frame work

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PaginationPratice::Application.config.secret_token = '20eed9f96d94aed32eee0a38e3294fbfc32b26c0d383bbfd8584e1dda9a5dfcb41693067e6aff9878a09b2f4f1deda3265622adaeea64a30ebe73f21dc85bc47'
