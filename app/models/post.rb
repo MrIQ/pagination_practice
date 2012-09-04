a117eb29f680068583764c8470fac36711a7a8cd
quchen <1987quchen@gmail.com>
2012-09-05 01:14:54 +0800

    add page_location_for method for Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,10 @@
 class Post < ActiveRecord::Base
   attr_accessible :content, :name
   validates :name, presence: true, uniqueness: true
+
+  def self.page_location_for(post)
+    post_index = Post.order(:created_at).to_a.index(post)
+    posts_per_page = WillPaginate.per_page
+    ((post_index*1.0+1)/posts_per_page).ceil
+  end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
 describe Post do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "@page_location_for" do
+    it "should tell me which page is a post on" do
+      new_post = FactoryGirl.create(:post, name: "latest")
+      FactoryGirl.create(:post, name: "middle")
+      old_post = FactoryGirl.create(:post, name: "older")
+      WillPaginate.per_page = 2
+      Post.page_location_for(old_post).should == 2
+      Post.page_location_for(new_post).should == 1
+    end
+  end
 end
